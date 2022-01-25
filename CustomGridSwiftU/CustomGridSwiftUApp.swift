6868180f30e68721fec51dfd5832ec5525677e4e
igor <igro.sia@yandex.ru>
2022-01-25 14:18:35 +0300

    Initial Commit

---
+++ CustomGridSwiftU/CustomGridSwiftUApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridSwiftUApp.swift
+//  CustomGridSwiftU
+//
+//  Created by Игорь Сысоев on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridSwiftUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
